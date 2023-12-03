8e2808061dd04b45da2e53d57eebd230fd9817a5
fumaxin <kalpeshdhoundiyal@gmail.com>
2023-12-03 09:51:21 +0530

    Play/Pause media script

---
+++ play_pause/README.md
@@ -0,0 +1,6 @@
+A bash script to play/pause any media running in background.
+
+Can add keyboard shorcut to exexute it through a button.
+
+Requirements:
+- Playerctl
---
+++ play_pause/media_toogle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if any media is playing
+status=$(playerctl status 2>&1)
+
+if [[ $status == "Playing" ]]; then
+    # If music is playing, pause it
+    playerctl pause
+    echo "Music paused"
+elif [[ $status == "Paused" ]]; then
+    # If music is paused, play it
+    playerctl play
+    echo "Music playing"
+else
+    # If no media is playing or paused, do something else or display a message
+    echo "No media found"
+fi
+
